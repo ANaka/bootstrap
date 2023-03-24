c41fc452f97a3a90eb5f62bd5c037ce55783c170
Alex Naka <alex.naka@gmail.com>
2023-03-24 12:18:20 -0700

    add dummy module and introspection tests

---
+++ bootstrap/dummy_module.py
@@ -0,0 +1,11 @@
+# dummy_module.py
+
+def dummy_function(a, b):
+    return a * b
+
+class DummyClass:
+    def dummy_method(self, x, y):
+        """
+        A dummy method for testing purposes.
+        """
+        return x * y
